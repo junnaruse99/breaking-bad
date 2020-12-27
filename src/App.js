7bc60a29c2902116aeb13d0f6e16df288aa71646
Jun Naruse <jun.naruse@pucp.edu.pe>
2020-12-26 23:57:18 -0500

    Small project just for testing a rest API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Phrase from './components/Phrase';
+import Spinner from './components/Spinner';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .3s ease;
+
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  margin-top: 20rem;
+`
+
+
 function App() {
+
+  // State of phrase
+  const [ phrase, savePhrase ] = useState({});
+
+  // State for loading
+  const [ loading, updateLoading ] = useState(true);
+
+
+  const consultAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const phrase = await api.json();
+    savePhrase(phrase[0]);
+    updateLoading(false);
+  }
+
+  // Load a phrase when document ready
+  useEffect( () => {
+    consultAPI();
+  }, []);
+
   return (
-    <div>
-    </div>
+    <Container>
+      {loading ? <SpinnerContainer><Spinner /></SpinnerContainer> : null}
+      <Phrase phrase={phrase} />
+      <Button
+        onClick={consultAPI}
+      >Get phrase</Button>
+    </Container>
   );
 }
 
---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import '../spinner.css'
+
+const Spinner = () => (
+    <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+    </div>
+)
+ 
+export default Spinner;
